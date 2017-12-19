b8595f593dc40950abf6e6a33dd278d8f90807b1
vescoc <vesco.claudio@PCVESCO.intranet.previmedical.it>
2017-12-19 09:42:01 +0100

    day 19

--- src/main/scala/aoc2017/Day19.scala
+++ src/main/scala/aoc2017/Day19.scala
@@ -15,38 +15,38 @@ object Day19 {
 
   def findStart(arr: Array[Array[Char]]) = (0, arr(0).indexOf('|'))
 
-  def walk(arr: Array[Array[Char]], str: String, count: Int, current: (Int, Int), direction: Direction): Option[String] = {
+  def walk(arr: Array[Array[Char]], str: String, count: Int, step: Int, current: (Int, Int), direction: Direction): Option[(Int, String)] = {
     //println(s"current=$str $current $direction $count")
 
     if (count == 0)
-      Some(str)
+      Some((step, str))
     else
       if (current._1 < 0 || current._1 >= arr.length || current._2 < 0 || current._2 > arr(current._1).length)
       None
     else
       arr(current._1)(current._2) match {
         case ' ' => None
-        case '|' if (direction == DOWN) => walk(arr, str, count - 1, (current._1 + 1, current._2), DOWN)
-        case '|' if (direction == UP) => walk(arr, str, count - 1, (current._1 - 1, current._2), UP)
-        case '|' if (direction == LEFT) => walk(arr, str, count, (current._1, current._2 - 1), LEFT)
-        case '|' if (direction == RIGHT) => walk(arr, str, count, (current._1, current._2 + 1), RIGHT)
-        case '-' if (direction == RIGHT) => walk(arr, str, count - 1, (current._1, current._2 + 1), RIGHT)
-        case '-' if (direction == LEFT) => walk(arr, str, count - 1, (current._1, current._2 - 1), LEFT)
-        case '-' if (direction == UP) => walk(arr, str, count, (current._1 - 1, current._2), UP)
-        case '-' if (direction == DOWN) => walk(arr, str, count, (current._1 + 1, current._2), DOWN)
-        case '+' if (direction == DOWN || direction == UP) => walk(arr, str, count - 1, (current._1, current._2 + 1), RIGHT) match {
+        case '|' if (direction == DOWN) => walk(arr, str, count - 1, step + 1, (current._1 + 1, current._2), DOWN)
+        case '|' if (direction == UP) => walk(arr, str, count - 1, step + 1, (current._1 - 1, current._2), UP)
+        case '|' if (direction == LEFT) => walk(arr, str, count, step + 1, (current._1, current._2 - 1), LEFT)
+        case '|' if (direction == RIGHT) => walk(arr, str, count, step + 1, (current._1, current._2 + 1), RIGHT)
+        case '-' if (direction == RIGHT) => walk(arr, str, count - 1, step + 1, (current._1, current._2 + 1), RIGHT)
+        case '-' if (direction == LEFT) => walk(arr, str, count - 1, step + 1, (current._1, current._2 - 1), LEFT)
+        case '-' if (direction == UP) => walk(arr, str, count, step + 1, (current._1 - 1, current._2), UP)
+        case '-' if (direction == DOWN) => walk(arr, str, count, step + 1, (current._1 + 1, current._2), DOWN)
+        case '+' if (direction == DOWN || direction == UP) => walk(arr, str, count - 1, step + 1, (current._1, current._2 + 1), RIGHT) match {
           case r @ Some(_) => r
-          case r @ _ => walk(arr, str, count - 1, (current._1, current._2 - 1), LEFT)
+          case r @ _ => walk(arr, str, count - 1, step + 1, (current._1, current._2 - 1), LEFT)
         }
-        case '+' if (direction == RIGHT || direction == LEFT) => walk(arr, str, count - 1, (current._1 + 1, current._2), DOWN) match {
+        case '+' if (direction == RIGHT || direction == LEFT) => walk(arr, str, count - 1, step + 1, (current._1 + 1, current._2), DOWN) match {
           case r @ Some(_) => r
-          case r @ _ => walk(arr, str, count - 1, (current._1 - 1, current._2), UP)
+          case r @ _ => walk(arr, str, count - 1, step + 1, (current._1 - 1, current._2), UP)
         }
         case c if (c == '+' || c == '|' || c == '+') => throw new MatchError(s"handle '$c' direction=$direction")
-        case l if (direction == DOWN) => walk(arr, str + l, count - 1, (current._1 + 1, current._2), DOWN)
-        case l if (direction == RIGHT) => walk(arr, str + l, count - 1, (current._1, current._2 + 1), RIGHT)
-        case l if (direction == UP) => walk(arr, str + l, count - 1, (current._1 - 1, current._2), UP)
-        case l if (direction == LEFT) => walk(arr, str + l, count - 1, (current._1, current._2 - 1), LEFT)
+        case l if (direction == DOWN) => walk(arr, str + l, count - 1, step + 1, (current._1 + 1, current._2), DOWN)
+        case l if (direction == RIGHT) => walk(arr, str + l, count - 1, step + 1, (current._1, current._2 + 1), RIGHT)
+        case l if (direction == UP) => walk(arr, str + l, count - 1, step + 1, (current._1 - 1, current._2), UP)
+        case l if (direction == LEFT) => walk(arr, str + l, count - 1, step + 1, (current._1, current._2 - 1), LEFT)
       }
   }
 
@@ -69,7 +69,7 @@ object Day19 {
 
       println(start)
 
-      println(walk(arr, "", target, start, DOWN))
+      println(walk(arr, "", target, 0, start, DOWN))
     }
 
     {
@@ -81,7 +81,7 @@ object Day19 {
 
       println(start)
 
-      println(walk(arr, "", target, start, DOWN))
+      println(walk(arr, "", target, 0, start, DOWN))
     }
   }
 }
