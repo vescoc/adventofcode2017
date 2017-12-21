06e163f0512785fb22e511d7d3d0510504400571
vescoc <claudio.vesco@gmail.com>
2017-12-21 17:46:38 +0100

    day 21 (not working)

--- src/main/scala/aoc2017/Day21.scala
+++ src/main/scala/aoc2017/Day21.scala
@@ -43,9 +43,102 @@ object Day21 {
     generate()
   }
 
-  def main(args: Array[String]) {
-    generate("...#") foreach println
+  def parse(lines: List[String]) = {
+    val re = """([.#/]+)\s+=>\s+([.#/]+)""".r
 
-    generate(".#...####") foreach println
+    def cleanup(str: String) = str.replaceAll("/", "")
+
+    lines
+      .map(line => line match {
+        case re(pattern, image) => {
+          val img = cleanup(image)
+          generate(cleanup(pattern)).map(pattern => (pattern -> img))
+        }
+      })
+      .flatten
+      .toMap
+  }
+
+  def display(str: String) = {
+    val l = math.sqrt(str.size).toInt
+    str.grouped(l) foreach println
+  }
+
+  def partition(image: String) = {
+    val size = image.size
+    val l = math.sqrt(size).toInt
+
+    def partition(s: Int, x: Int, y: Int) =
+      (for (i <- 0 until s) yield image.slice(y * l * s + x * s + i * l, y * l * s + x * s + i * l + s)).mkString
+
+    def iterate(pl: Int) = 
+      (
+        for {
+          y <- 0 until pl
+          x <- 0 until pl
+        } yield partition(pl, x, y)
+      ).toList
+
+    if (l <= 3)
+      List(image)
+    else if (l % 2 == 0) {
+      iterate(l / 2)
+    } else if (l % 3 == 0) {
+      iterate(l / 3)
+    } else
+      throw new MatchError(s"invalid value for $l")
+  }
+
+  def reassemble(list: List[String]) = {
+    val image = list.mkString
+
+    val l = math.sqrt(list.head.size).toInt
+    val s = math.sqrt(list.size).toInt
+
+    (
+      for {
+        y <- 0 until s
+        i <- 0 until l
+        x <- 0 until s
+      } yield list(x + y * s).slice(i * l, i * l + l)
+    ).mkString
+  }
+
+  def draw(book: Map[String, String], limit: Int, image: String) = {
+    @tailrec
+    def iterate(step: Int = 0, current: String = image): String = {
+      println(s"step=$step")
+      display(current)
+
+      if (step == limit)
+        current
+      else {
+        val partitions = partition(current)
+        println(partitions)
+        iterate(step + 1, reassemble(partition(current).map(image => book(image))))
+      }
+    }
+
+    iterate()
+  }
+
+  def main(args: Array[String]) {
+    val book = parse(Source.fromFile("data/day21/input.txt").getLines.toList)
+
+    val start = ".#...####"
+
+    book filter(_._1.size == 4) foreach println
+
+    println("")
+
+    display(start)
+
+    println("")
+
+    display(book(start))
+
+    println("")
+
+    display(draw(book, 5, start))
   }
 }
