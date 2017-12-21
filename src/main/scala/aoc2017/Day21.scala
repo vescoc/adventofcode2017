c671fa8ea63552070fec5907abb1f4faf6d829e2
vescoc <claudio.vesco@gmail.com>
2017-12-21 22:32:34 +0100

    resolved day21

--- src/main/scala/aoc2017/Day21.scala
+++ src/main/scala/aoc2017/Day21.scala
@@ -4,6 +4,8 @@ import scala.annotation.tailrec
 
 import scala.io.Source
 
+import Benchmark._
+
 object Day21 {
   def at(pattern: String)(p: Int) = pattern.charAt(p).toString
 
@@ -19,11 +21,12 @@ object Day21 {
           case _ => throw new MatchError(s"invalid size for $pattern -> ${pattern.size}")
         }
       }
+
       def rotate(pattern: String): String = {
         val at = Day21.at(pattern)(_)
 
         pattern.size match {
-          case 4 => at(3) + at(0) + at(1) + at(2)
+          case 4 => at(2) + at(0) + at(3) + at(1)
           case 9 => at(6) + at(3) + at(0) + at(7) + at(4) + at(1) + at(8) + at(5) + at(2)
           case _ => throw new MatchError(s"invalid size for $pattern -> ${pattern.size}")
         }
@@ -56,7 +59,13 @@ object Day21 {
         }
       })
       .flatten
-      .toMap
+      .foldLeft[Map[String, String]](Map())((m, p) => {
+        if (m.contains(p._1)) {
+          println(s"conflics $p old ${m(p._1)}")
+          m
+        } else
+          m + p
+      })
   }
 
   def display(str: String) = {
@@ -71,50 +80,51 @@ object Day21 {
     def partition(s: Int, x: Int, y: Int) =
       (for (i <- 0 until s) yield image.slice(y * l * s + x * s + i * l, y * l * s + x * s + i * l + s)).mkString
 
-    def iterate(pl: Int) = 
+    def iterate(pl: Int, size: Int) =
       (
         for {
           y <- 0 until pl
           x <- 0 until pl
-        } yield partition(pl, x, y)
+        } yield partition(size, x, y)
       ).toList
 
     if (l <= 3)
       List(image)
     else if (l % 2 == 0) {
-      iterate(l / 2)
+      iterate(l / 2, 2)
     } else if (l % 3 == 0) {
-      iterate(l / 3)
+      iterate(l / 3, 3)
     } else
-      throw new MatchError(s"invalid value for $l")
+        throw new MatchError(s"invalid value for $l")
   }
 
   def reassemble(list: List[String]) = {
-    val image = list.mkString
+    if (list.size == 1)
+      list.head
+    else {
+      val image = list.mkString
 
-    val l = math.sqrt(list.head.size).toInt
-    val s = math.sqrt(list.size).toInt
+      val l = math.sqrt(list.head.size).toInt
+      val s = math.sqrt(list.size).toInt
 
-    (
-      for {
-        y <- 0 until s
-        i <- 0 until l
-        x <- 0 until s
-      } yield list(x + y * s).slice(i * l, i * l + l)
-    ).mkString
+      (
+        for {
+          y <- 0 until s
+          i <- 0 until l
+          x <- 0 until s
+        } yield list(x + y * s).slice(i * l, i * l + l)
+      ).mkString
+    }
   }
 
   def draw(book: Map[String, String], limit: Int, image: String) = {
     @tailrec
     def iterate(step: Int = 0, current: String = image): String = {
-      println(s"step=$step")
-      display(current)
-
       if (step == limit)
         current
       else {
+        println(s"step $step ${current.size}")
         val partitions = partition(current)
-        println(partitions)
         iterate(step + 1, reassemble(partition(current).map(image => book(image))))
       }
     }
@@ -123,22 +133,35 @@ object Day21 {
   }
 
   def main(args: Array[String]) {
-    val book = parse(Source.fromFile("data/day21/input.txt").getLines.toList)
+    val inputBook = parse(Source.fromFile("data/day21/input.txt").getLines.toList)
+    val testBook = parse(List("../.# => ##./#../...", ".#./..#/### => #..#/..../..../#..#"))
 
     val start = ".#...####"
 
-    book filter(_._1.size == 4) foreach println
-
-    println("")
-
+    println("start")
     display(start)
 
-    println("")
+    println("test book")
+    testBook foreach println
 
-    display(book(start))
+    println("test part1")
+    display(draw(testBook, 2, start))
 
-    println("")
+    println("target .#.#=" + generate(".#.#"))
+    println("input .##.=" + generate(".##."))
+    println("input ##..=" + generate("##.."))
 
-    display(draw(book, 5, start))
+    bench("part1") {
+      val part1 = draw(inputBook, 5, start)
+      println("part1 info " + part1.size + " " + math.sqrt(part1.size))
+      println("part1 result=" + part1.filter(c => c == '#').size)
+    }
+
+    if (true)
+      bench("part2") {
+        val part2 = draw(inputBook, 18, start)
+        println("part2 info " + part2.size + " " + math.sqrt(part2.size))
+        println("part2 result=" + part2.filter(c => c == '#').size)
+      }
   }
 }
